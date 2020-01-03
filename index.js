214c5988ccefad2855be1f095904ff3f517366e7
Patrick <pateast678@gmail.com>
2020-01-03 14:50:36 -0500

    already did it, but now its committed

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+    console.log('Oh wow I got a request')
+    console.log(req.url);
+    console.log(req.method);
+
+    let url = req.url;
+    if(url === '/') {
+        url = '/index.html';
+    }
+
+    // Read the HTML file's contents so we can send it in the response
+    fs.readFile(`${__dirname}/public${url}`, (error, buf)=> {
+        if(error) {
+            console.log(error);
+            res.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+            res.end(`<h1>404</h1><h1>File not found</h1>`);
+        } else{
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            const contents = buf.toString();
+            res.end(contents);
+        }
+    });
+
+});
+
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000, dude.')
+});
